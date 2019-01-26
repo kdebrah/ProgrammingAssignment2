57f3cf241c079bb782c14b906392f6bd2dc93488
kofidebrah <kofidebrah24@gmail.com>
2019-01-26 12:42:17 +0000

    Update cachematrix.R
    This is the coursera R programming week 3 peer review assignment. It is function that  creates a special matrix object that can cache and compute the inverse of the matrix.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions here create a special matrix object that can cache and compute the inverse of the matrix
 
-## Write a short comment describing this function
+## This function creates the special matrix and caches its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+          m <- NULL
+          set <- function(y) {
+            x <<- y
+            m <<- NULL
+          }
+          get <- function() x
+          setmatrixInverse <- function(matrixInverse) m <<- matrixInverse
+          getmatrixInverse <- function() m
+          list(set=set, get=get, setmatrixInverse=setmatrixInverse, getmatrixInverse=getmatrixInverse)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special matrix, if the inverse has already been calcutated, then the inverse is retreived from the cache
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...){
+          m <- x$getmatrixInverse()
+          if (!is.null(m)){
+            message("getting cached data")
+            return(m)
+            }                            ## Return a matrix that is the inverse of 'x'
+          data <- x$get()
+          m <- solve(data)
+          x$setmatrixInverse(m)
+          m
 }
